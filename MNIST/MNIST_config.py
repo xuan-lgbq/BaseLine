c7b47ed293b41f5701a8849f90f48e59d8a49a29
“xuan” <bxl307@student.bham.ac.uk>
2025-04-09 15:31:19 +0800

    Applying tiny space set up

---
+++ MNIST/MNIST_config.py
@@ -0,0 +1,41 @@
+# config.py
+import torch
+import numpy as np
+import wandb
+
+# 定义设备
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+elif torch.backends.mps.is_available():
+    device = torch.device("mps")
+else:
+    device = torch.device("cpu")
+
+
+# 随机种子
+np_seed = 12138
+torch_seed = 12138
+
+np.random.seed(np_seed)
+torch.manual_seed(torch_seed)
+
+
+# 超参数
+config = {
+    "np_seed": 12138,
+    "torch_seed": 12138,
+    "learning_rate": 0.01,
+    "batch_size": 50, # 添加 Batch Size [Source: 1452 in 2405.16002v3.pdf]
+    "steps": 20000, # 增加步数以匹配论文图1的范围 (原为500) 
+    #调整记录步数以适应更长的训练
+    "record_steps": [100, 500, 1000, 2500, 5000, 10000, 15000, 20000],
+    "input_dim": 784,
+    "hidden_dim": 200,
+    "output_dim": 10,
+    "train_samples": 5000,
+    "test_samples": 500,
+    "top_k_pca_number": 10, # 论文中通常取 k=类别数=10 或 2，这里保持 5 或改为 10
+    "wandb_project_name": "MNIST",
+    # Updated run name to reflect setup
+    "wandb_run_name": "3 NN MLP+Tanh+SGD+MSE+MNIST-5k(tiny space set up)"
+}
