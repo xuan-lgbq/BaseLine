6bf7a980c37edec17a26e559b1850a3b8764b338
Can He <18760793593@163.com>
2025-05-04 19:01:42 +0800

    Update CIFAR_config.py

--- CIFAR/CIFAR_config.py
+++ CIFAR/CIFAR_config.py
@@ -22,13 +22,13 @@ torch.manual_seed(torch_seed)
 
 # 超参数
 config = {
-    "np_seed": 0,
-    "torch_seed": 0,
+    "np_seed": 42,
+    "torch_seed": 42,
     "learning_rate": 0.001,
     "batch_size": 50, # 添加 Batch Size [Source: 1452 in 2405.16002v3.pdf]
-    "steps": 1000,  #此处先暂定
+    "steps": 25000,  #此处先暂定
     #调整记录步数以适应更长的训练
-    "record_steps": [100, 200, 300, 400, 500, 600, 700, 800],
+    "record_steps": [5000,10000,15000,20000],
     "input_dim": 3072, # CIFAR-10 images are 32x32x3 = 3072
     "flatten_dim": 2048, #32 × 8 × 8
     "output_dim": 10,
@@ -37,5 +37,5 @@ config = {
     "top_k_pca_number": 10, 
     "wandb_project_name": "CIFAR",
     # Updated run name to reflect setup
-    "wandb_run_name": "test-CNN+ReLU+SGD+MSE+CIFAR10-5K(tiny space set up)"
+    "wandb_run_name": "batch-CNN+ReLU+SGD+MSE+CIFAR10-5K(tiny space set up)"
 }
